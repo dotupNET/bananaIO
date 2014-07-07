35ddeadaea35d72c4b25ee9fd3fd8d69ebbc88d9
pi <pullrich@DOTUP-NB6.dotup.local>
2014-07-07 11:30:08 +0200

    Initial commit

---
+++ bananaIO/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: pullrich
+ *
+ * Created on 7. Juli 2014, 11:19
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return (EXIT_SUCCESS);
+}
+
